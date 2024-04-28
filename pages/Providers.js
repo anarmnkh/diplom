5d77bb89f70dedad67dc7469aeb749830c3dc80e
Anar09 <78798117+Anar09@users.noreply.github.com>
2024-04-29 00:55:41 +0800

    a

--- pages/Providers.js
+++ pages/Providers.js
@@ -3,4 +3,4 @@ import { SessionProvider } from "next-auth/react";
 export const AuthProvider = ({ children }) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
-export default AuthProvider;
+export default AuthProvider;    
